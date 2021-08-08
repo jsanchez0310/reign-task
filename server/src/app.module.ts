c252703aee6f67b2e32f75acba72d0d25c17589c
José Sanchez <jsanchez.waku@gmail.com>
2021-08-07 23:10:53 -0400

    Server Component y MongoDB dockerized

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,12 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ScheduleModule } from '@nestjs/schedule';
 import { ArticlesModule } from './articles/articles.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      ignoreEnvFile: true,
+    }),
     ArticlesModule,
-    MongooseModule.forRoot('mongodb://jasm:secretpass@localhost:27017', {
+    MongooseModule.forRoot(process.env.MONGODB_URI, {
       dbName: 'articles',
       useFindAndModify: false,
     }),
